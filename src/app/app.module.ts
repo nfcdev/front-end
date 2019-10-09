e04370c16189b72fbe5cc397944ef7543325e4b4
Daniel <danbr894@student.liu.se>
2019-10-09 10:30:22 +0200

    Revert "Created angular components for the check in form"
    This reverts commit e01ae7a3eef17e13132ecc468b965b86c14a1227.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CheckInFormComponent } from './check-in-form/check-in-form.component';
-import { CheckInFormDialogComponent } from './check-in-form-dialog/check-in-form-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CheckInFormComponent,
-    CheckInFormDialogComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatDialogModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
