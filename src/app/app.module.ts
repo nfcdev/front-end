cb858fffedc920979191d656933fae6c180d2960
oskho10 <oskar.holmstrom@liu.se>
2019-10-15 14:27:45 +0200

    Angular material table created and added to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayArticleDataComponent } from './display-article-data/display-article-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableArticleDataComponent } from './table-article-data/table-article-data.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayArticleDataComponent
+    DisplayArticleDataComponent,
+    TableArticleDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-  ],
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
