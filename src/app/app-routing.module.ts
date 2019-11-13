b10b923d5d42d983e9fd9cbdb315afcdce121898
Martinsvanberg <marsv123@student.liu.se>
2019-11-13 15:56:46 +0100

    Implemented checkboxes for the article data table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
-
+import { TableArticleDataComponent } from "./table-article-data/table-article-data.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent}, // Sets the starting page to login
+  { path: '', component: TableArticleDataComponent}, // Sets the starting page to the material table
   { path: 'main', component: MainComponent }
 ];
 
