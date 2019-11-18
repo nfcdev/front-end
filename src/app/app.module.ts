f99b5979c4ee88967efbecc916143b93fba4be42
Gustav Aaro <gustav.aaro@gmail.com>
2019-11-18 11:06:35 +0100

    Fix merge issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CheckInFormComponent, CheckInFormDialogComponent } from './check-in-form/check-in-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TableArticleDataComponent } from './table-article-data/table-article-data.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { StorageEventFormComponent, StorageEventFormDialogComponent } from './storage-event-form/storage-event-form.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import {
+  CheckInFormComponent,
+  CheckInFormDialogComponent
+} from "./check-in-form/check-in-form.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TableArticleDataComponent } from "./table-article-data/table-article-data.component";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import {
+  StorageEventFormComponent,
+  StorageEventFormDialogComponent
+} from "./storage-event-form/storage-event-form.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from "./login/login.component";
 
-import { CheckInDropDownComponent } from './check-in-drop-down/check-in-drop-down.component';
-import { MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatGridListModule } from '@angular/material';
-import { MainComponent } from './main/main.component';
-import { UnauthorizedInterceptor } from './auth/unauthorized.interceptor';
-import { AuthenticationService } from './auth/authService';
-import { MaterialPageComponent, MaterialPageDialogComponent } from './material-page/material-page.component';
-import { PackagePageComponent, PackagePageDialogComponent } from './package-page/package-page.component';
-import { ShelfPageComponent, ShelfPageDialogComponent } from './shelf-page/shelf-page.component';
+import { CheckInDropDownComponent } from "./check-in-drop-down/check-in-drop-down.component";
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatButtonModule,
+  MatGridListModule,
+  MatMenuModule
+} from "@angular/material";
+import { MainComponent } from "./main/main.component";
+import { UnauthorizedInterceptor } from "./auth/unauthorized.interceptor";
+import { AuthenticationService } from "./auth/authService";
+import {
+  MaterialPageComponent,
+  MaterialPageDialogComponent
+} from "./material-page/material-page.component";
+import {
+  PackagePageComponent,
+  PackagePageDialogComponent
+} from "./package-page/package-page.component";
+import {
+  ShelfPageComponent,
+  ShelfPageDialogComponent
+} from "./shelf-page/shelf-page.component";
 
 @NgModule({
   declarations: [
@@ -81,10 +103,12 @@ import { ShelfPageComponent, ShelfPageDialogComponent } from './shelf-page/shelf
     AuthenticationService
   ],
   bootstrap: [AppComponent],
-  entryComponents: [CheckInFormDialogComponent,
-     StorageEventFormDialogComponent,
-     MaterialPageDialogComponent,
-     PackagePageDialogComponent,
-     ShelfPageDialogComponent]
+  entryComponents: [
+    CheckInFormDialogComponent,
+    StorageEventFormDialogComponent,
+    MaterialPageDialogComponent,
+    PackagePageDialogComponent,
+    ShelfPageDialogComponent
+  ]
 })
 export class AppModule {}
