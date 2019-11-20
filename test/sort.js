36719b0504df1ff0fc0d287ba038331fb48ea9b1
Simon Runefors <simru092@student.liu.se>
2019-11-20 12:38:14 +0100

    working on test to sort, not knowing how to validate if right sorted

--- test/sort.js
+++ test/sort.js
@@ -17,7 +17,7 @@ const puppeteer = require('puppeteer');
     page = await browser.newPage();
   }); */
 (async () => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
     
     await page.goto('http://192.168.99.100:9001/main')
@@ -25,11 +25,18 @@ const puppeteer = require('puppeteer');
     
     await page.setViewport({ width: 1920, height: 937 })
     await page.screenshot({ path: 'before_sort_article.png' })
+
+   /*  await page.waitForSelector('.ng-star-inserted > div > app-check-in-form > .mat-raised-button > .mat-button-wrapper')
+  await page.click('.ng-star-inserted > div > app-check-in-form > .mat-raised-button > .mat-button-wrapper')
+  
+  await page.waitForSelector('.mat-form-field #mat-input-4')
+  await page.click('.mat-form-field #mat-input-4') */
     
-    await page.waitForSelector('thead > .mat-header-row > .ng-tns-c9-0 > .mat-sort-header-container > .mat-sort-header-button')
-    await page.click('thead > .mat-header-row > .ng-tns-c9-0 > .mat-sort-header-container > .mat-sort-header-button')
-    await page.screenshot({ path: 'after_sort_article.png' })
-    await browser.close()
-  })();
+  await page.setViewport({ width: 1920, height: 937 })
   
+  await page.waitForSelector('thead > .mat-header-row > .ng-tns-c14-2 > .mat-sort-header-container > .mat-sort-header-button')
+  await page.click('thead > .mat-header-row > .ng-tns-c14-2 > .mat-sort-header-container > .mat-sort-header-button')
+  await page.screenshot({ path: 'after_sort_article.png' })
   
+  await browser.close()
+  })()
