47776dbacbe990934cd292894ae5f404eaba2df5
Filip Sehlin <filipsehlin@hotmail.com>
2019-11-24 01:04:31 +0100

    Searchfunction working with applying and removing filters. Only possible to search for reference, material and storage room so far.

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,7 +1,10 @@
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { applyFilter, removeFilter } from '../table-article-data/table-article-data.component';
+import { TableArticleDataItem } from '../table-article-data/table-article-data-datasource';
+import EXAMPLE_DATA from '../table-article-data/example_data.json';
+
 
 // This creates the type "option" which collects the data from the search
 export interface Option {
@@ -14,7 +17,8 @@ export interface Option {
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.less']
 })
-export class SearchBarComponent  {
+
+export class SearchBarComponent implements OnInit {
   visible = true;
   selectable = true;
   removable = true;
@@ -23,6 +27,17 @@ export class SearchBarComponent  {
   options: Option[] = [];
   activeMaterials: Boolean = true;
   inactiveMaterials: Boolean = false;
+  tableData: TableArticleDataItem [] = EXAMPLE_DATA; // TODO: adjust for actual data
+  storageRooms: String [] = [];
+
+  ngOnInit() {
+    // Adds all of the storage storageRooms
+    for (let i = 0; i < this.tableData.length; i++) {
+      if ((this.storageRooms.includes(this.tableData[i].storage_room))==false) {
+        this.storageRooms.push(this.tableData[i].storage_room)
+      }
+    }
+  }
 
   add(event: MatChipInputEvent): void {
     const input = event.input;
@@ -33,19 +48,24 @@ export class SearchBarComponent  {
     if ((value || '').trim()) {
       // The following functions categorise the input option and is currently based on the "example_data.json"
       // TODO: Update according to fit the actual data.
+      /** IMPORTANT! The name of the tempCategories created below are used in "table-article-data.component.ts" as well,
+       *  so if they are changed here they need to altered in that file also*/ 
 
       // Reference number
       if ((value || '').trim().length == 6) {
-        tempCategory = "reference_number";
+        tempCategory = "Fall";
       }
       // Material number
       if ((value || '').trim().length == 2) {
-        tempCategory = "material_number";
+        tempCategory = "Material";
+      }
+      if (this.storageRooms.includes((value || '').trim())) {
+        tempCategory = "Avdelning"
       }
       var tempOption: Option =  { name: value.trim(), category: tempCategory }
       this.options.push({ name: value.trim(), category: tempCategory });
+      applyFilter(tempOption);
     }
-
     // Reset the input value
     if (input) {
       input.value = '';
@@ -54,20 +74,10 @@ export class SearchBarComponent  {
 
   remove (option: Option):void {
     const index = this.options.indexOf(option);
-
     if (index >=0) {
       this.options.splice(index, 1);
+      removeFilter(this.options)
     }
   }
-
-  onSubmit ():void {
-    // TODO: handle the output and filter the table
-    
-    const size = this.options.length;
-    for (let i =0; i < size; i++ ){
-      console.log(this.options[i]);
-    }
-    this.options.splice(0, size);
-  }
-
 }
+
--- src/app/table-article-data/table-article-data.component.ts
+++ src/app/table-article-data/table-article-data.component.ts
@@ -6,6 +6,49 @@ import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { TableArticleDataDataSource, TableArticleDataItem } from './table-article-data-datasource';
 import { MaterialCheckBoxService } from './material-check-box.service';
+import { Option } from '../search-bar/search-bar.component';
+
+var dataSource: TableArticleDataDataSource;
+var dataConstant: TableArticleDataItem [];
+export function applyFilter(filter: Option) {
+  var tempData: TableArticleDataItem[] = [];
+  if (filter.category=="Fall") {
+    for (let j = 0; j < dataSource.data.length; j++) {
+      console.log(j, dataSource.data.length);
+      if (dataSource.data[j].reference_number == filter.name) {
+        tempData.push(dataSource.data[j]);
+        console.log("Found one!")
+      }
+    }
+    dataSource.data = tempData;
+  }
+  if (filter.category=="Material") {
+    for (let j = 0; j < dataSource.data.length; j++) {
+      if (dataSource.data[j].material_number == filter.name) {
+        tempData.push(dataSource.data[j]);
+      }
+    }
+    dataSource.data = tempData;
+  }
+  if (filter.category=="Avdelning") {
+    for (let j = 0; j < dataSource.data.length; j++) {
+      if (dataSource.data[j].storage_room == filter.name) {
+        tempData.push(dataSource.data[j]);
+      }
+    }
+    dataSource.data = tempData;
+  }
+  console.log(dataSource.data);
+  // TODO: Some way to refresh the table.
+}
+
+export function removeFilter(filters: Option []) {
+  dataSource.data = dataConstant;
+  for (let i = 0; i < filters.length; i++) {
+    applyFilter(filters[i]);
+  }
+}
+
 
 @Component({
   selector: 'app-table-article-data',
@@ -16,7 +59,7 @@ export class TableArticleDataComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: false}) sort: MatSort;
   @ViewChild(MatTable, {static: false}) table: MatTable<TableArticleDataItem>;
-  dataSource: TableArticleDataDataSource;
+  // dataSource: TableArticleDataDataSource;
   selection = new SelectionModel(true, []);
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
@@ -26,31 +69,33 @@ export class TableArticleDataComponent implements AfterViewInit, OnInit {
   constructor(
     private materialCheckOutService: MaterialCheckBoxService
   ) { }
-/** Whether the number of selected elements matches the total number of rows. */
-isAllSelected() {
-  const numSelected = this.selection.selected.length;
-  const numRows = this.dataSource.data.length;
-  return numSelected == numRows;
-  
-}
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = dataSource.data.length;
+    return numSelected == numRows;
+    
+  }
 
-/** Selects all rows if they are not all selected; otherwise clear selection. */
-masterToggle() {
-  this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
-}
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        dataSource.data.forEach(row => this.selection.select(row));
+  }
 
   ngOnInit() {
-    this.dataSource = new TableArticleDataDataSource();
+    dataSource = new TableArticleDataDataSource();
+    dataConstant = dataSource.data;
     this.selection.changed.subscribe(newSelection => {
       this.materialCheckOutService.update(this.selection);
     });
+    console.log(dataSource.data);
   }
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    dataSource.sort = this.sort;
+    dataSource.paginator = this.paginator;
+    this.table.dataSource = dataSource;
   }
 }
