d84a8d52073cf8681cabba23cb5a0161cdc09338
Filip Sehlin <filipsehlin@hotmail.com>
2019-11-27 21:18:48 +0100

    View only mode fixed. When a user (who is not an admin) logs in from and undefined location the user cannot use any of the buttons except for visualization

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from "@angular/core";
+import { StorageRoomStore, StorageRoom } from '../storage-room/storage-room-store';
+import { StorageRoomService } from '../storage-room/storage-room.service';
+import { AuthenticationService } from '../auth/authService';
 
 @Component({
   selector: "app-main",
   templateUrl: "./main.component.html",
-  styleUrls: ["./main.component.less"]
+  styleUrls: ["./main.component.less"],
+  providers: [StorageRoomService]
 })
 export class MainComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private storageRoomService: StorageRoomService,
+    private storageRoomStore: StorageRoomStore,
+    private authService: AuthenticationService) {}  
 
-  ngOnInit() {}
+  viewOnly: Boolean = false;
+  isAdmin: Boolean = false;
+
+  ngOnInit() {
+    this.authService.user.subscribe(user => {
+      this.isAdmin = this.authService.isUserAdmin();
+    });
+    let storageRooms: StorageRoom[] = [];
+    this.storageRoomService.getStorageRooms().subscribe(res => {
+      storageRooms = res;
+    })
+    this.storageRoomStore.currentStorageRoom.subscribe(res => {
+      if (!res.id && !this.isAdmin) {
+        this.viewOnly = true;
+      }
+    })
+        console.log("Viewonly = " + this.viewOnly);
+  }
 }
