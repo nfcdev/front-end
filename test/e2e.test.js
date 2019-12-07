09250be6bc3e9c682b0a4740aeff3f8472ff8791
Lojdquist <axel.lojdquist@vizlib.com>
2019-12-07 13:54:35 +0100

    add more e2e test

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
 const puppeteer = require('puppeteer');
 const { expect } = require('chai');
@@ -12,6 +13,12 @@ console.log(`Connecting to host url: ${HOST_URL}`);
 let browser;
 let page;
 
+function delay(time) {
+  return new Promise(((resolve) => {
+    setTimeout(resolve, time);
+  }));
+}
+
 before(async () => {
   browser = await puppeteer.launch({
     headless: false,
@@ -23,37 +30,106 @@ before(async () => {
     ],
   });
   page = await browser.newPage();
+  await page.goto(HOST_URL, { waitUntil: 'networkidle2' });
 });
+
 
 describe('Test Website meta information', () => {
   it('page title should be FrontEnd', async () => {
-    await page.setViewport({
-      width: 1000,
-      height: 1000,
-    });
-
-    await page.goto(HOST_URL, { waitUntil: 'networkidle2' });
-
     const pageTitle = await page.title();
-
-    expect(pageTitle).to.equal('FrontEnd');
+    expect(pageTitle).to.be.equal('FrontEnd');
   });
 });
 
-
 describe('Login', () => {
   it('User should be able to login', async () => {
-    await page.goto(HOST_URL, { waitUntil: 'networkidle2' });
+    await page.waitForSelector('input[name="kortkod"]');
+    await page.type('input[name="kortkod"]', 'admon');
 
+    await page.waitForSelector('span[class="mat-button-wrapper"]');
+    await page.click('span[class="mat-button-wrapper"]');
+  });
+});
 
-    await page.waitForSelector('a[href="http://localhost:9000/login"]');
-    await page.click('a[href="http://localhost:9000/login"]');
+describe('Search bar', async () => {
+  it('Should search for "111111"', async () => {
+    await page.waitForSelector('input[formcontrolname="searchcomponent"]');
+    await page.type('input[formcontrolname="searchcomponent"]', '111111\n');
+  });
 
-    await page.waitForSelector('input[name="username"]');
-    await page.type('input[name="username"]', 'user1');
-    await page.type('input[name="password"]', 'user1pass');
+  it('Should test that search was completed', async () => {
+    await page.waitForSelector('mat-chip[class="mat-chip mat-primary mat-standard-chip mat-chip-with-trailing-icon ng-star-inserted"]');
+    element = await page.$('mat-chip[class="mat-chip mat-primary mat-standard-chip mat-chip-with-trailing-icon ng-star-inserted"]');
+    expect('mat-chip[class="mat-chip mat-primary mat-standard-chip mat-chip-with-trailing-icon ng-star-inserted"]').not.to.be.null;
+  });
 
-    await page.waitForSelector('button[tabindex="6"]');
-    await page.click('button[tabindex="6"]');
+  it('Should remove the search and return', async () => {
+    await page.waitForSelector('button[id="clear"]');
+    await page.click('button[id="clear"]');
+    element = await page.$('mat-chip[class="mat-chip mat-primary mat-standard-chip mat-chip-with-trailing-icon ng-star-inserted"]');
+    expect(element).to.be.null;
+  });
+});
+
+describe('Buttons', async () => {
+  it('Should test button "Hantera System"', async () => {
+    await page.waitForSelector('.row > .button > app-manage-system-dialog > .mat-raised-button > .mat-button-wrapper');
+    await page.click('.row > .button > app-manage-system-dialog > .mat-raised-button > .mat-button-wrapper');
+    await delay(1500);
+    await page.keyboard.down('Escape');
+  });
+
+  it('Should test button "Visualisering"', async () => {
+    await page.waitForSelector('.row > .button > app-data-visualization > .mat-raised-button > .mat-button-wrapper');
+    await page.click('.row > .button > app-data-visualization > .mat-raised-button > .mat-button-wrapper');
+    await delay(1500);
+    await page.keyboard.down('Escape');
+  });
+
+  it('Should testing "Check-in + package"-button (no check in should be completed)', async () => {
+    await page.waitForSelector('app-check-in-drop-down');
+    await page.click('app-check-in-drop-down');
+
+    await page.waitForSelector('.mat-menu-panel > .mat-menu-content > app-package-check-in > .mat-menu-item');
+    await page.click('.mat-menu-panel > .mat-menu-content > app-package-check-in > .mat-menu-item');
+
+    await page.waitForSelector('input[formcontrolname="reference_number"]');
+    await page.type('input[formcontrolname="reference_number"]', '111111\n');
+    await page.keyboard.down('Escape');
+  });
+
+  it('Should testing "Check-in + material"-button(no check in should be completed)', async () => {
+    await page.waitForSelector('app-check-in-drop-down');
+    await page.click('app-check-in-drop-down');
+    await page.waitForSelector('.mat-menu-panel > .mat-menu-content > app-material-check-in > .mat-menu-item');
+    await page.click('.mat-menu-panel > .mat-menu-content > app-material-check-in > .mat-menu-item');
+
+    await page.waitForSelector('input[formcontrolname="material_number"]');
+    await page.type('input[formcontrolname="material_number"]', '111111-01\n');
+    await page.keyboard.down('Escape');
+  });
+
+  it('Should test button "Checka ut + package" (no check out should be performed)', async () => {
+    await page.waitForSelector('div > .row > .button > .mat-menu-trigger > .mat-button-wrapper');
+    await page.click('div > .row > .button > .mat-menu-trigger > .mat-button-wrapper');
+
+    await page.waitForSelector('app-package-check-out');
+    await page.click('app-package-check-out');
+
+    await page.waitForSelector('input[formcontrolname="package"]');
+    await page.type('input[formcontrolname="package"]', '111111-K01\n');
+    await page.keyboard.down('Escape');
+  });
+
+  it('Should test button "Checka ut + material" (no check out should be performed)', async () => {
+    await page.waitForSelector('div > .row > .button > .mat-menu-trigger > .mat-button-wrapper');
+    await page.click('div > .row > .button > .mat-menu-trigger > .mat-button-wrapper');
+
+    await page.waitForSelector('app-material-check-out');
+    await page.click('app-material-check-out');
+
+    await page.waitForSelector('input[formcontrolname="material_number"]');
+    await page.type('input[formcontrolname="material_number"]', '111111\n');
+    await page.keyboard.down('Escape');
   });
 });
